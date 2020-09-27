b1720a2f8172a054819ecb990433a3376506fe38
simeonreusch <simeon.reusch@desy.de>
2020-09-27 17:50:15 +0200

    added ZTF field info CGI parser

--- plan.py
+++ plan.py
@@ -2,19 +2,21 @@
 # Author: Simeon Reusch (simeon.reusch@desy.de)
 # License: BSD-3-Clause
 
+import time, os
+import astropy
+from astropy.time import Time
+from astropy import units as u
+from astropy.coordinates import SkyCoord
 import os, time
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import astroplan as ap
-import astropy
-from astropy import units as u
-from astropy.coordinates import SkyCoord
-from astropy.time import Time
 from astroplan import Observer
 from datetime import datetime
 from astroplan.plots import plot_airmass, plot_altitude
-
+import requests
+from bs4 import BeautifulSoup
 
 class ObservationPlan:
     def __init__(
@@ -51,7 +53,7 @@ class ObservationPlan:
             self.palomar,
             self.now,
             brightness_shading=True,
-            altitude_yaxis=True,
+            altitude_yaxis=False,
             max_airmass=2.5,
         )
         ax.axvline(Time(self.now).plot_date, color="black", label="now", ls="dotted")
@@ -66,11 +68,65 @@ class ObservationPlan:
         plt.savefig(f"{self.name}_airmass.png")
 
 
-RA = 96.46
-DEC = -4.33
+    def request_ztf_fields(self):
+        URL = "http://yupana.caltech.edu/cgi-bin/ptf/tb//zoc"
+        IMAGE_URL_1 = "http://yupana.caltech.edu/marshals/tb//igmo_0_0.png"
+        IMAGE_URL_2 = "http://yupana.caltech.edu/marshals/tb//igmo_0_1.png"
+        IMAGE_URLS = [IMAGE_URL_1, IMAGE_URL_2]
+
+        objra = self.ra
+        objdec = self.dec
+        radius = 60
+
+        fieldids_total = []
+
+        for grid in [1, 2]:
+
+            request_data = {
+            "showobject": 1, 
+            "objra": objra,
+            "objdec": objdec,
+            "grid": grid,
+            "objname": "unknown",
+            "radam": radius,
+            "submitshowobject": "SUBMIT (Show Object)",
+            }
+
+            # Post the request
+            # response = requests.post(url=URL, data=request_data, timeout=30)
+            response = requests.get(URL, params=request_data)
+            img_data = requests.get(IMAGE_URL_1).content
+            with open('test.png', 'wb') as handler:
+                handler.write(img_data)
+
+            soup = BeautifulSoup(response.text, 'html5lib')
+
+            # try:
+            pre = soup.find_all('pre')[-1]
+            results = pre.text.split("\n")[1:-3]
+            fieldids = []
+            for result in results:
+                if len(result) > 10:
+                    fieldid = int(result[0:7])
+                    fieldids.append(fieldid)
+                    fieldids_total.append(fieldid)
+
+            for index, fieldid in enumerate(fieldids):
+                img_data = requests.get(IMAGE_URLS[index]).content
+                with open(f"grid_{fieldid}.png", 'wb') as handler:
+                    handler.write(img_data)
+                    
+        print(f"Fields that are possible: {fieldids_total}")
+
+# NEED TO INCLUDE ERROR CIRCLE CALCULATION
+# RA = 96.46
+# DEC = -4.33
+RA = 40
+DEC = 45
 NAME = "IC200926A"
 ARRIVALTIME = "2020-09-26 07:54:11.621"
 
 plan = ObservationPlan(ra=RA, dec=DEC, name=NAME, arrivaltime=ARRIVALTIME)
 
-plan.plot_target()
+# plan.plot_target()
+plan.request_ztf_fields()
