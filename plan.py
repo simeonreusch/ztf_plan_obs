f668ea6a26980de711dc536b838828fae06440e8
simeonreusch <simeon.reusch@desy.de>
2020-10-05 12:53:43 +0200

    various improvements

--- plan.py
+++ plan.py
@@ -6,7 +6,7 @@ import time, os, warnings
 import astropy
 from astropy.time import Time
 from astropy import units as u
-from astropy.coordinates import SkyCoord
+from astropy.coordinates import SkyCoord, AltAz
 import os, time
 import numpy as np
 import matplotlib.pyplot as plt
@@ -46,11 +46,15 @@ class ObservationPlan:
 
         self.coordinates = SkyCoord(self.ra * u.deg, self.dec * u.deg, frame="icrs")
         self.coordinates_galactic = self.coordinates.galactic
-        print(self.coordinates_galactic)
         self.target = ap.FixedTarget(name=self.name, coord=self.coordinates)
         self.palomar = Observer.at_site("Palomar", timezone="US/Pacific")
         self.now = Time(datetime.utcnow())
         self.date = date
+        # if self.date is not None:
+        #     moon_coords = astropy.coordinates.get_moon(time=Time(self.date + " 00:00:00.000000"), location=self.palomar.location)
+        # else:
+        #     moon_coords = astropy.coordinates.get_moon(time=self.now, location=self.palomar.location)
+        # self.moon = ap.FixedTarget(name="moon", coord=moon_coords)
 
         if self.date is not None:
             self.start_obswindow = Time(self.date + " 00:00:00.000000")
@@ -68,7 +72,17 @@ class ObservationPlan:
             ap.AtNightConstraint.twilight_astronomical(),
         ]
 
+        # Obtain moon coordinates at Palomar for the full time window
         times = Time(self.start_obswindow + np.linspace(0, 24, 1000) * u.hour)
+        moon_times = Time(self.start_obswindow + np.linspace(0, 24, 50) * u.hour)
+        moon_coords = []
+
+        for time in moon_times:
+            moon_coord = astropy.coordinates.get_moon(
+                time=time, location=self.palomar.location
+            )
+            moon_coords.append(moon_coord)
+        self.moon = moon_coords
 
         airmass = self.palomar.altaz(times, self.target).secz
         airmass = np.ma.array(airmass, mask=airmass < 1)
@@ -106,8 +120,6 @@ class ObservationPlan:
         distance_to_evening = min_airmass_time.mjd - twilight_evening.mjd
         distance_to_morning = twilight_morning.mjd - min_airmass_time.mjd
 
-        print(f"Minimal airmass ({min_airmass:.2f}) at {min_airmass_time}")
-
         if distance_to_morning < distance_to_evening:
             self.g_band_recommended_time_start = (
                 min_airmass_time - 300 * u.s - 0.5 * u.hour
@@ -131,6 +143,10 @@ class ObservationPlan:
             self.r_band_recommended_time_end = (
                 self.r_band_recommended_time_start + 300 * u.s
             )
+        print(f"RADEC = {self.coordinates.ra.deg} {self.coordinates.dec.deg}")
+        print(f"Minimal airmass ({min_airmass:.2f}) at {min_airmass_time}")
+        print("Separation from galactic plane:")
+        print(f"{self.coordinates_galactic.b.deg:.2f} deg")
 
         print("Recommended observation times:")
         print(
@@ -166,36 +182,39 @@ class ObservationPlan:
         else:
             time = self.now
 
-        ax = plot_airmass(
+        ax = plot_altitude(
             self.target,
             self.palomar,
             time,
             brightness_shading=True,
-            altitude_yaxis=False,
-            max_airmass=2.5,
+            min_altitude=10,
         )
+
+        # Plot a vertical line for the current time
         ax.axvline(Time(self.now).plot_date, color="black", label="now", ls="dotted")
+
+        # Plot a vertical line for the neutrino arrival time if available
         if self.arrivaltime is not None:
             ax.axvline(
                 Time(self.arrivaltime).plot_date,
                 color="indigo",
                 label="neutrino arrival",
-                ls="dotted",
+                ls="dashed",
             )
 
         start, end = ax.get_xlim()
 
         plt.text(
             start,
-            0.8,
-            f"Recommended observation times:\n\ng-band: {self.time_shortener(self.g_band_recommended_time_start)} --- {self.time_shortener(self.g_band_recommended_time_end)} [UTC]\n r-band: {self.time_shortener(self.r_band_recommended_time_start)} --- {self.time_shortener(self.r_band_recommended_time_end)} [UTC]",
+            100,
+            f"RADEC = {self.coordinates.ra.deg} {self.coordinates.dec.deg}\nSeparation from Galactic Plane: {self.coordinates_galactic.b.deg:.2f} deg\nRecommended observation times:\ng-band: {self.time_shortener(self.g_band_recommended_time_start)} --- {self.time_shortener(self.g_band_recommended_time_end)} [UTC]\n r-band: {self.time_shortener(self.r_band_recommended_time_start)} --- {self.time_shortener(self.r_band_recommended_time_end)} [UTC]",
         )
 
         if self.date is not None:
             ax.set_xlabel(f"{self.date} [UTC]")
         else:
             ax.set_xlabel(f"{self.now.datetime.date()} [UTC]")
-        plt.grid(True, color="brown", linestyle="dotted", which="both")
+        plt.grid(True, color="gray", linestyle="dotted", which="both", alpha=0.5)
 
         ax.axvspan(
             self.g_band_recommended_time_start.plot_date,
@@ -211,17 +230,61 @@ class ObservationPlan:
             color="red",
         )
 
+        # Now we plot the moon altitudes and separation
+        moon_altitudes = []
+        moon_times = []
+        moon_separations = []
+        for moon in self.moon:
+            moonalt = moon.transform_to(
+                AltAz(obstime=moon.obstime, location=self.palomar.location)
+            ).alt.deg
+            moon_altitudes.append(moonalt)
+            moon_times.append(moon.obstime.plot_date)
+            separation = moon.separation(self.coordinates).deg
+            moon_separations.append(separation)
+        ax.plot(
+            moon_times,
+            moon_altitudes,
+            color="orange",
+            linestyle=(0, (1, 2)),
+            label="moon",
+        )
+
+        # And we annotate the separations
+
+        for i, moonalt in enumerate(moon_altitudes):
+            if moonalt > 20 and i % 3 == 0:
+                ax.annotate(
+                    f"{moon_separations[i]:.0f}",
+                    xy=(moon_times[i], moonalt),
+                    textcoords="data",
+                    fontsize=6,
+                )
+
+        # for i, xy in enumerate(zip(moon_times, moon_altitudes)):
+        #     ax.annotate(f"{moon_separations[i]:.0f}", xy=xy, textcoords='data')
+
         x = np.linspace(start + 0.03, end + 0.03, 9)
 
         # Add recommended upper limit for airmass
-        y = np.full((len(x), 1), 2)
-        ax.errorbar(x, y, 0.05, color="red", uplims=True, fmt=" ")
+        y = np.full((len(x), 1), 30)
+        ax.errorbar(x, y, 2, color="red", lolims=True, fmt=" ")
+
+        # Plot an airmass scale
+        ax2 = ax.secondary_yaxis(
+            "right", functions=(self.altitude_to_airmass, self.airmass_to_altitude)
+        )
+        altitude_ticks = np.linspace(10, 90, 9)
+        airmass_ticks = np.round(self.altitude_to_airmass(altitude_ticks), 2)
+        ax2.set_yticks(airmass_ticks)
+        ax2.set_ylabel("Airmass")
 
         plt.tight_layout()
+        plt.legend()
         outpath_png = os.path.join(self.name, f"{self.name}_airmass.png")
         outpath_pdf = os.path.join(self.name, f"{self.name}_airmass.pdf")
-        plt.savefig(outpath_png)
-        plt.savefig(outpath_pdf)
+        plt.savefig(outpath_png, dpi=300, bbox_inches="tight")
+        plt.savefig(outpath_pdf, bbox_inches="tight")
 
         # NOTE: INCLUDE MOON AND SUN IN THIS!
 
@@ -303,19 +366,32 @@ class ObservationPlan:
         time_short = str(time)[:-7] + ":00"
         return time_short
 
+    @staticmethod
+    def airmass_to_altitude(altitude):
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            airmass = 90 - np.degrees(np.arccos(1 / altitude))
+        return airmass
+
+    @staticmethod
+    def altitude_to_airmass(airmass):
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            altitude = 1.0 / np.cos(np.radians(90 - airmass))
+        return altitude
+
 
 # NEED TO INCLUDE ERROR CIRCLE CALCULATION
 NAME = "IC200929A"
 # RA = 90.46
 # DEC = -4.33
 # ARRIVALTIME = "2020-09-26 07:54:11.621"
-date = "2020-09-30"
+# date = "2020-09-29"
 
-plan = ObservationPlan(
-    name=NAME, date=date
-)  # , ra=RA, dec=DEC, arrivaltime=ARRIVALTIME
+plan = ObservationPlan(name=NAME)  # , date=date
+# , ra=RA, dec=DEC, arrivaltime=ARRIVALTIME
 
 # )  # , date=date)
 
 plan.plot_target()
-plan.request_ztf_fields()
+# plan.request_ztf_fields()
