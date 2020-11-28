bf25f72787d7d2c44a45dce74555e266379c6782
simeonreusch <simeon.reusch@desy.de>
2020-11-28 16:18:49 +0100

    versionchange

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ MAINTAINER = "Simeon Reusch"
 MAINTAINER_EMAIL = "simeon.reusch@desy.de"
 URL = "https://github.com/simeonreusch/ztf_plan_obs/"
 LICENSE = "BSD (3-clause)"
-DOWNLOAD_URL = "https://github.com/simeonreusch/ztf_plan_obs/archive/v0.31.tar.gz"
-VERSION = "0.31"
+DOWNLOAD_URL = "https://github.com/simeonreusch/ztf_plan_obs/archive/v0.32.tar.gz"
+VERSION = "0.32"
 
 try:
     from setuptools import setup, find_packages
--- ztf_plan_obs/__init__.py
+++ ztf_plan_obs/__init__.py
@@ -1,1 +1,1 @@
-__version__ = "v0.31"
+__version__ = "v0.32"
